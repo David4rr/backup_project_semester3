22e79c2217168e683ca5f8335b20f48754945c47
David <davidarrozaqi085@gmail.com>
2022-12-05 14:58:23 +0700

    Revisi chart to Dropdown

--- lib/pages/selamatdatang_pages.dart
+++ lib/pages/selamatdatang_pages.dart
@@ -15,6 +15,7 @@ class _selamatdatangState extends State<selamatdatang> {
 
   @override
   Widget build(BuildContext context) {
+    final MediaQueryData mediaQuery = MediaQuery.of(context);
     return Scaffold(
       resizeToAvoidBottomInset: true,
       backgroundColor: primaryColor,
@@ -67,23 +68,29 @@ class _selamatdatangState extends State<selamatdatang> {
                                 children: [
                                   //* Bagian Modal
                                   Container(
-                                      color: Colors.transparent,
+                                    color: Colors.transparent,
+                                    child: Container(
+                                      decoration: const BoxDecoration(
+                                          gradient: LinearGradient(
+                                            begin: Alignment.topLeft,
+                                            end: Alignment.bottomRight,
+                                            colors: [
+                                              Color(0xFF16D682),
+                                              Color(0xFF05BE5E)
+                                            ],
+                                          ),
+                                          borderRadius: BorderRadius.only(
+                                              topRight: Radius.circular(40),
+                                              topLeft: Radius.circular(40))),
                                       child: Container(
-                                        decoration: const BoxDecoration(
-                                            gradient: LinearGradient(
-                                              begin: Alignment.topLeft,
-                                              end: Alignment.bottomRight,
-                                              colors: [
-                                                Color(0xFF16D682),
-                                                Color(0xFF05BE5E)
-                                              ],
-                                            ),
-                                            borderRadius: BorderRadius.only(
-                                                topRight: Radius.circular(40),
-                                                topLeft: Radius.circular(40))),
-                                        child: Container(
-                                          margin: EdgeInsets.symmetric(
-                                              horizontal: defaultmargin),
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: defaultmargin),
+                                        // margin: EdgeInsets.symmetric(
+                                        //     horizontal: defaultmargin),
+                                        child: Padding(
+                                          padding: EdgeInsets.only(
+                                              bottom:
+                                                  mediaQuery.viewInsets.bottom),
                                           child: Column(
                                             crossAxisAlignment:
                                                 CrossAxisAlignment.start,
@@ -265,6 +272,7 @@ class _selamatdatangState extends State<selamatdatang> {
                                               const SizedBox(
                                                 height: 10,
                                               ),
+
                                               SizedBox(
                                                   height: 60,
                                                   width: MediaQuery.of(context)
@@ -316,7 +324,9 @@ class _selamatdatangState extends State<selamatdatang> {
                                             ],
                                           ),
                                         ),
-                                      ))
+                                      ),
+                                    ),
+                                  )
                                 ],
                               );
                             });
